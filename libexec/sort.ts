1b352478f2c13a42777a2f3b34f924e046760713
Max Howell <mxcl@me.com>
2023-02-10 10:11:36 -0500

    wip

--- libexec/sort.ts
+++ libexec/sort.ts
@@ -8,16 +8,27 @@ args:
   - --allow-env=TEA_PREFIX,TEA_PANTRY_PATH
 ---*/
 
+import { parseFlags } from "cliffy/flags/mod.ts"
 import { parse, str } from "utils/pkg.ts"
 import { usePantry } from "hooks"
 import { hydrate } from "prefab"
 
+const { flags: { d: separator }, unknown: args } = parseFlags(Deno.args, {
+  flags: [{
+    name: "d",
+    default: "\n"
+  }, {
+    name: "test"
+  }],
+})
+
+
 const pantry = usePantry()
 
-const dry = Deno.args.map(parse)
+const dry = args.map(parse)
 const wet = await hydrate(dry, async (pkg, dry) => {
   const deps = await pantry.getDeps(pkg)
   return dry ? [...deps.build, ...deps.runtime] : deps.runtime
 })
 
-console.log(wet.dry.map(str).join("\n"))
+console.log(wet.dry.map(str).join(separator))
