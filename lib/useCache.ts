cc617a37abffdc50a2b8344b6a82b64f11ae2f64
uesyn <17411645+uesyn@users.noreply.github.com>
2023-05-01 17:28:25 +0900

    Support short versions

--- lib/useCache.ts
+++ lib/useCache.ts
@@ -6,31 +6,42 @@ export default function() {
   return { decode }
 }
 
+const bottleRegex = `^(.*)-(\\d+[:alpha:]*?(.\\d+[:alpha:]*?)*)\\+(.+?)\\+(.+?)\\.tar\\.[gx]z$`
+const srcRegex = `^(.*)-(\\d+[:alpha:]*?(.\\d+[:alpha:]*?)*)\\.tar\\.[gx]z$`
+
 function decode(path: Path): Stowed | undefined {
-  const match = path.basename().match(`^(.*)-(\\d+\\.\\d+\\.\\d+.*?)(\\+(.+?)\\+(.+?))?\\.tar\\.[gx]z$`)
-  if (!match) return
-    const [_, p, v, host, platform, arch] = match
+  const bottleMatch = path.basename().match(bottleRegex)
+  if (bottleMatch) {
+    const [_1, p, v, _2, platform, arch] = bottleMatch
     // Gotta undo the package name manipulation to get the package from the bottle
     const project = p.replaceAll("∕", "/")
-    const version = new SemVer(v)
+    const version = new SemVer(v, { tolerant: true })
     if (!version) return
     const pkg = { project, version }
-    if (host) {
-      const compression = path.extname() == '.tar.gz' ? 'gz' : 'xz'
-      return {
-        pkg,
-        type: 'bottle',
-        host: {
-          platform: platform as SupportedPlatform,
-          arch: arch as SupportedArchitecture
-        },
-        compression,
-        path
-      }
-    } else {
-      return {
-        pkg, type: 'src', path,
-        extname: path.extname(),
-      }
+    const compression = path.extname() == '.tar.gz' ? 'gz' : 'xz'
+    return {
+      pkg,
+      type: 'bottle',
+      host: {
+        platform: platform as SupportedPlatform,
+        arch: arch as SupportedArchitecture
+      },
+      compression,
+      path
     }
+  }
+
+  const srcMatch = path.basename().match(srcRegex)
+  if (srcMatch) {
+    const [_, p, v] = srcMatch
+    // Gotta undo the package name manipulation to get the package from the bottle
+    const project = p.replaceAll("∕", "/")
+    const version = new SemVer(v, { tolerant: true })
+    if (!version) return
+    const pkg = { project, version }
+    return {
+      pkg, type: 'src', path,
+      extname: path.extname(),
+    }
+  }
 }
