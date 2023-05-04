5bbf6949057e67520989d22c8a8c714300db99f6
uesyn <17411645+uesyn@users.noreply.github.com>
2023-05-05 03:48:33 +0900

    Fix regex for uploading and add the tests

--- tests/unit/useCache.test.ts
+++ tests/unit/useCache.test.ts
@@ -6,16 +6,22 @@ import SemVer from "semver"
 Deno.test("decode", async test => {
   const cache = useCache()
 
-  const versions = ["1", "1a", "1.2", "1.2a", "1.2.3", "1.2.3a", "1.2.3.4", "1.2.3.4a"] as const
+  const versions = ["1", "9e" /* pattern of ijg.org */, "1.2", "3.3a" /* pattern of tmux */, "1.2.3", "1.2.3a", "1.2.3.4", "1.2.3.4a"] as const
+  const archs = ["x86-64", "aarch64"]
+  const platforms = ["linux", "darwin"]
   for (const version of versions) {
-    await test.step(`type == bottle, version == ${version}`, () => {
-      const stowed = cache.decode(Path.root.join(`project∕foo-${version}+linux+x86-64.tar.gz`)) as any
-      assertEquals(stowed.pkg.project, "project/foo")
-      assertEquals(stowed.pkg.version, new SemVer(version, { tolerant: true }))
-      assertEquals(stowed.type, "bottle")
-      assertEquals(stowed.compression, "gz")
-      assertEquals(stowed.host, {arch: "x86-64", platform: "linux"})
-    })
+    for (const arch of archs) {
+      for (const platform of platforms) {
+        await test.step(`type == bottle, arch == ${arch}, platform == ${platform}, version == ${version}`, () => {
+          const stowed = cache.decode(Path.root.join(`project∕foo-${version}+${platform}+${arch}.tar.gz`)) as any
+          assertEquals(stowed.pkg.project, "project/foo")
+          assertEquals(stowed.pkg.version, new SemVer(version, { tolerant: true }))
+          assertEquals(stowed.type, "bottle")
+          assertEquals(stowed.compression, "gz")
+          assertEquals(stowed.host, {arch: arch, platform: platform})
+        })
+      }
+    }
   }
 
   for (const version of versions) {
