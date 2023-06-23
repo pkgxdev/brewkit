5bcb6ba64b2e6eb05b20e7cd3d71a06dc40009ab
Jacob Heider <jacob@tea.xyz>
2023-06-23 16:22:06 -0400

    add some checks to project name

--- libexec/audit.ts
+++ libexec/audit.ts
@@ -2,6 +2,7 @@
 
 import { parseFlags } from "cliffy/flags/mod.ts"
 import { hooks, utils } from "tea"
+import { swallow } from "../lib/utils.ts"
 
 const { useCellar, usePantry, useMoustaches } = hooks
 const { parse } = utils.pkg
@@ -22,6 +23,29 @@ for(const pkg of pkgnames.map(parse)) {
     const bin = path.join('bin', name)
     const sbin = path.join('sbin', name)
     if (!bin.isExecutableFile() && !sbin.isExecutableFile()) missing.push([pkg.project, name])
+  }
+
+  // Enforce some naming conventions
+
+  // project must be a FQDN
+  if (!pkg.project.match(/\./) || swallow(() => new URL(`https://${pkg.project}`)) === undefined) {
+    console.error(`error: ${pkg.project} is not a valid project name. Please use a fully qualified URL to the canonical project page.`)
+    Deno.exit(1)
+  // project should not start with www
+  } else if (pkg.project.match(/^www\./)) {
+    console.error(`error: ${pkg.project} *is* a valid project name, but you started with \`www\`, which we find unsightly. Maybe try \`${pkg.project.replace(/^www\./, '')}\`?`)
+    Deno.exit(1)
+  // project should not start with a protocol
+  // note that this isn't possible on a mac, as far as I can tell, but better safe than sorry.
+  } else if (pkg.project.match(/^(ht|f)tps?:\/\//)) {
+    console.error(`error: ${pkg.project} *is* a valid project name, but you started with a protocol, which we would prefer not. Maybe try \`${pkg.project.replace(/^(ht|f)tps?:\/\//, '')}\`?`)
+    Deno.exit(1)
+  // project name should only contain letters, numbers, dashes, underscores, dots, and slashes
+  // this might be handled by the URL check above, but better safe than sorry. Certainly, URLs
+  // can have colons, question marks, percent signs and ampersands.
+  } else if (pkg.project.match(/[^a-zA-Z0-9\-_\.\/]/)) {
+    console.error(`error: ${pkg.project} contains characters we don't like. Maybe try \`${pkg.project.replace(/[^a-zA-Z0-9\-_\.\/]/g, '')}\`?`)
+    Deno.exit(1)
   }
 }
 
