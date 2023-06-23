d933d68a2f9b5d9a54747ec0fff9a343bbc39c43
Jacob Heider <jacob@tea.xyz>
2023-06-23 16:24:37 -0400

    reduce repetition

--- libexec/audit.ts
+++ libexec/audit.ts
@@ -29,27 +29,27 @@ for(const pkg of pkgnames.map(parse)) {
 
   // project must be a FQDN
   if (!pkg.project.match(/\./) || swallow(() => new URL(`https://${pkg.project}`)) === undefined) {
-    console.error(`error: ${pkg.project} is not a valid project name. Please use a fully qualified URL to the canonical project page.`)
-    Deno.exit(1)
+    fail(`error: ${pkg.project} is not a valid project name. Please use a fully qualified URL to the canonical project page.`)
   // project should not start with www
   } else if (pkg.project.match(/^www\./)) {
-    console.error(`error: ${pkg.project} *is* a valid project name, but you started with \`www\`, which we find unsightly. Maybe try \`${pkg.project.replace(/^www\./, '')}\`?`)
-    Deno.exit(1)
+    fail(`error: ${pkg.project} *is* a valid project name, but you started with \`www\`, which we find unsightly. Maybe try \`${pkg.project.replace(/^www\./, '')}\`?`)
   // project should not start with a protocol
   // note that this isn't possible on a mac, as far as I can tell, but better safe than sorry.
   } else if (pkg.project.match(/^(ht|f)tps?:\/\//)) {
-    console.error(`error: ${pkg.project} *is* a valid project name, but you started with a protocol, which we would prefer not. Maybe try \`${pkg.project.replace(/^(ht|f)tps?:\/\//, '')}\`?`)
-    Deno.exit(1)
+    fail(`error: ${pkg.project} *is* a valid project name, but you started with a protocol, which we would prefer not. Maybe try \`${pkg.project.replace(/^(ht|f)tps?:\/\//, '')}\`?`)
   // project name should only contain letters, numbers, dashes, underscores, dots, and slashes
   // this might be handled by the URL check above, but better safe than sorry. Certainly, URLs
   // can have colons, question marks, percent signs and ampersands.
   } else if (pkg.project.match(/[^a-zA-Z0-9\-_\.\/]/)) {
-    console.error(`error: ${pkg.project} contains characters we don't like. Maybe try \`${pkg.project.replace(/[^a-zA-Z0-9\-_\.\/]/g, '')}\`?`)
-    Deno.exit(1)
+    fail(`error: ${pkg.project} contains characters we don't like. Maybe try \`${pkg.project.replace(/[^a-zA-Z0-9\-_\.\/]/g, '')}\`?`)
   }
 }
 
 if (missing.length) {
-  console.error(`error: missing executables:\n${missing.map(([pkg, provide]) => pkg + ' => ' + provide).join('\n')}`)
+  fail(`error: missing executables:\n${missing.map(([pkg, provide]) => pkg + ' => ' + provide).join('\n')}`)
+}
+
+function fail(msg: string) {
+  console.error(msg)
   Deno.exit(1)
 }
