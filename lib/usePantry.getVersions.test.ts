936da0a206040d4d5b8885105a589416e19c9658
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2023-10-24 22:51:42 +0530

    feat(usePantry): Make `versions` take `(PlainObject | string) | (PlainObject | string)[]`. (#192)
    * feat(usePantry): Make `versions` to be `(PlainObject | string) | (PlainObject | string)[]`.
    
    * test(usePantry): Add additional tests.
    
    * apply changes
    
    * fix tests
    
    * add tests and necessary permissions
    
    * add GH_TOKEN to deno test

--- lib/usePantry.getVersions.test.ts
+++ lib/usePantry.getVersions.test.ts
@@ -15,3 +15,17 @@ Deno.test("single version", async () => {
   assert(foo[0].eq(new SemVer("3.0.0")))
   assertEquals(foo.length, 1)
 })
+
+Deno.test("complex versions", async () => {
+  const foo = await _parse([
+    {
+      github: "rust-lang/rls/tags"
+    },
+    "1.0.1",
+    "1.0.2"
+  ])
+  assert(foo[0].eq(new SemVer("0.125.0"))); // First RLS version
+  assert(foo[foo.length - 3].eq(new SemVer("1.39.0"))) // RLS is no longer maintained and v1.39.0 is the last version available
+  assert(foo[foo.length - 2].eq(new SemVer("1.0.1")))
+  assert(foo[foo.length - 1].eq(new SemVer("1.0.2")))
+})
