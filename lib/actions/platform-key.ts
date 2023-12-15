d588330e2b52300873b060a84208304175314c49
Max Howell <mxcl@me.com>
2023-12-15 12:27:32 -0500

    Fixes corruption of artifacts when racing (#225)
    Using v3 rather v4 of v3 of (upload|download)-artifact because https://github.com/actions/upload-artifact/issues

--- lib/actions/platform-key.ts
+++ lib/actions/platform-key.ts
@@ -1,5 +1,21 @@
-#!/usr/bin/env -S pkgx deno run --allow-env
+#!/usr/bin/env -S pkgx deno run --allow-env --allow-read
 
+import { Command } from "cliffy/command/mod.ts"
+import get_config from '../config.ts'
 import { utils } from 'pkgx'
-const { arch, platform } = utils.host()
-console.log(`${platform}+${arch}`)
+
+let { options: { pkg, platform } } = await new Command()
+  .option('--pkg=[type:string]', 'Package name')
+  .option('--platform=[type:string]', 'Platform name')
+  .parse(Deno.args);
+
+if (platform === true) platform = undefined
+// ^^ unfortunate that we need to accept empty string due to GITHUB ACTIONS
+// BEING SUPER GOOD and Cliffy then takes that to mean “true” as it
+// interprets it as FLAG ON
+platform ??= (({arch, platform}) => `${platform}+${arch}`)(utils.host())
+if (pkg === true) pkg = undefined
+
+const config = await get_config(pkg as string | undefined)
+
+console.log(`${encodeURIComponent(utils.pkg.str(config.pkg))}+${platform}`)
--- lib/actions/stage.ts
+++ lib/actions/stage.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env -S pkgx deno run --allow-read --allow-env --allow-write
 
-import { hooks, utils, Path } from "pkgx"
+import { hooks, Path } from "pkgx"
+import get_config from '../config.ts'
 
-const pkg = utils.pkg.parse(Deno.args[1]?.trim() || Deno.env.get('BREWKIT_PKGSPEC')!)
+const config = await get_config(Deno.args[1])
+
+const pkg = config.pkg
 const { path } = await hooks.useCellar().resolve(pkg)
 
 const stage = new Path(Deno.args[0])
