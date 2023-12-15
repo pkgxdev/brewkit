c9565e024805e05a2ff0e66597be24009dde8ad3
Max Howell <mxcl@me.com>
2023-12-15 07:21:47 -0500

    Fixes corruption of artifacts when racing

--- lib/actions/platform-key.ts
+++ lib/actions/platform-key.ts
@@ -1,5 +1,16 @@
 #!/usr/bin/env -S pkgx deno run --allow-env
 
+import { Command } from "cliffy/command/mod.ts"
+import get_config from '../config.ts'
 import { utils } from 'pkgx'
-const { arch, platform } = utils.host()
-console.log(`${platform}+${arch}`)
+
+let { options: { pkg, platform } } = await new Command()
+  .option('--pkg=<pkg>', 'Package name')
+  .option('--platform=<platform>', 'Platform name')
+  .parse(Deno.args);
+
+platform ??= ((arch, platform) => `${arch}+${platform}`)(utils.host())
+
+const config = await get_config(pkg)
+
+console.log(`${utils.pkg.str(config.pkg)}+${platform}`)
