1839017a4484c2ed24d7038b1bb32d8ddef498c3
Max Howell <mxcl@me.com>
2023-12-15 10:43:44 -0500

    wip

--- lib/actions/platform-key.ts
+++ lib/actions/platform-key.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S pkgx deno run --allow-env
+#!/usr/bin/env -S pkgx deno run --allow-env --allow-read
 
 import { Command } from "cliffy/command/mod.ts"
 import get_config from '../config.ts'
