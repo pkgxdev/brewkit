a4dc74d9a83f2a7479a1d7aad8b805f883638faa
Max Howell <mxcl@me.com>
2023-12-26 16:01:24 +0000

    [build robustness] Remove -Werror from cc args (#247)
    This flag is for dev, not production. Remove it.

--- projects/toolchain.com/foo.c
+++ projects/toolchain.com/foo.c
@@ -2,6 +2,9 @@
 #include "zlib.h"
 
 int main() {
+  int a;
+  a += 1; // creates a warning that we can use with -Werror arg removal
+
   z_stream strm = {0};
   inflateInit(&strm);
   printf("sup_bro\n");
