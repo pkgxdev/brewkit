13bb48525868cc45c832f8597285fc6d488bda96
Max Howell <mxcl@me.com>
2024-02-05 10:21:48 -0500

    Fixes #303

--- build/build.ts
+++ build/build.ts
@@ -175,7 +175,7 @@ function make_toolchain() {
         const path = d.join(name)
         if (path.exists()) continue
         const target = useConfig().prefix.join('llvm.org/v*/bin', to)
-        path.ln('s', { target })
+        path.rm().ln('s', { target })
       }
     }
 
